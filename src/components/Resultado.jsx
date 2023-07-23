fca355fa1871827051bee716f967c4683612ea7c
BrunoSantos <111144170+BrunoSantosprogrammer@users.noreply.github.com>
2023-07-23 18:27:10 -0300

    feat: add classe dinamica ao componente resultado

--- src/components/Resultado.jsx
+++ src/components/Resultado.jsx
@@ -1,11 +1,16 @@
+import classNames from "classnames"
+
 export const Resultado = () => {
+  const cn = classNames("resultado", {
+    "resultado--aberto": false
+  })
   return (
-    <div className="resultado">
-      <div className="resultado__box">
+    <div className={cn}>
+      <div className="resultado__caixa">
         <p>Seu nível de memória é:</p>
         <h1>Bom</h1>
         <img src="/kitekat-3.png" alt="Imagem referente ao nível de memória" height={150}/>
-        <p>
+        <p className="resultado__porcentos">
           <strong>Taxa de acertos: </strong>
           <span>60%</span>
         </p>
